a16fe5777921726ec04c2b7dc5410250faff5338
PyRPy <44626163+PyRPy@users.noreply.github.com>
2020-06-22 16:43:55 -0500

    explore the stocks potentials

---
+++ techAnalysis/td_20200622_ZM.R
@@ -0,0 +1,47 @@
+# reference from youtube 
+# https://www.youtube.com/channel/UC0Amh8UkHvIVxPbxsrEBRMg
+# load packages
+library(quantmod)
+library(PerformanceAnalytics)
+# Stock watch list --------------------------------------------------------
+
+# a collection of stocks
+mystocks <- c("GE", "JNJ", "MSFT", "WMT", "GS", "SBUX", "ZM", "^DJI")
+
+# read data
+getSymbols(mystocks, from = "2015-01-01", src = "yahoo")
+# getSymbols('ZM',src='csv')
+# getSymbols('MSFT',src='csv')
+
+# Daily return and charts -------------------------------------------------
+
+lineChart(DJI, subset = '2015::2020')
+addSMA(n = 10, col = "white")
+addSMA(n = 50, col = "blue")
+addSMA(n = 200, col = "red")
+zoomChart(DJI, subset = "2020")
+
+candleChart(MSFT, subset = "2020")
+addSMA(n = 10, col = "white")
+addSMA(n = 50, col = "blue")
+addSMA(n = 200, col = "red")
+addRSI(n = 14, maType = "EMA")
+addBBands(n = 20, sd = 2)
+addMACD(fast = 12, slow = 26, signal = 9, type = "EMA")
+
+candleChart(WMT, subset = "2020")
+addSMA(n = 10, col = "white")
+addSMA(n = 50, col = "blue")
+addSMA(n = 200, col = "red")
+addRSI(n = 14, maType = "EMA")
+addBBands(n = 20, sd = 2)
+addMACD(fast = 12, slow = 26, signal = 9, type = "EMA")
+
+candleChart(ZM, subset = "2020")
+addSMA(n = 10, col = "white")
+addSMA(n = 50, col = "blue")
+addSMA(n = 200, col = "red")
+addRSI(n = 14, maType = "EMA")
+addBBands(n = 20, sd = 2)
+addMACD(fast = 12, slow = 26, signal = 9, type = "EMA")
+zoomChart(ZM, subset = "2020-06")
