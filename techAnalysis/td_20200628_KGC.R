0e53c251155b5445590a2b546ccc6fcc848b807b
PyRPy <44626163+PyRPy@users.noreply.github.com>
2020-06-28 21:30:54 -0500

    Create td_20200628_KGC.R
    gold related stocks scanning

---
+++ techAnalysis/td_20200628_KGC.R
@@ -0,0 +1,113 @@
+# reference from youtube 
+# https://www.youtube.com/channel/UC0Amh8UkHvIVxPbxsrEBRMg
+# load packages
+library(quantmod)
+library(PerformanceAnalytics)
+# Stock watch list --------------------------------------------------------
+
+# a collection of stocks
+mystocks <- c("MSFT", "WMT")
+
+# read data
+getSymbols(mystocks, from = "2015-01-01", src = "yahoo")
+# getSymbols('ZM',src='csv')
+# getSymbols('MSFT',src='csv')
+
+# Daily return and charts -------------------------------------------------
+
+# 2020-06-28 --------------------------------------------------------------
+
+candleChart(MSFT, subset = "2020")
+addSMA(n = 10, col = "white")
+addSMA(n = 50, col = "blue")
+addSMA(n = 200, col = "red")
+addRSI(n = 14, maType = "EMA")
+addBBands(n = 20, sd = 2)
+addMACD(fast = 12, slow = 26, signal = 9, type = "EMA")
+
+candleChart(WMT, subset = "2020") # buy
+addSMA(n = 10, col = "white")
+addSMA(n = 50, col = "blue")
+addSMA(n = 200, col = "red")
+addRSI(n = 14, maType = "EMA")
+addBBands(n = 20, sd = 2)
+addMACD(fast = 12, slow = 26, signal = 9, type = "EMA")
+
+getSymbols("KGC", from = "2015-01-01", src = "yahoo")
+candleChart(KGC, subset = "2020") # 
+addSMA(n = 10, col = "white")
+addSMA(n = 50, col = "blue")
+addSMA(n = 200, col = "red")
+addRSI(n = 14, maType = "EMA")
+addBBands(n = 20, sd = 2)
+addMACD(fast = 12, slow = 26, signal = 9, type = "EMA")
+lineChart(KGC)
+
+getSymbols("AUY", from = "2015-01-01", src = "yahoo") # 
+candleChart(AUY, subset = "2020")
+addSMA(n = 10, col = "white")
+addSMA(n = 50, col = "blue")
+addSMA(n = 200, col = "red")
+addRSI(n = 14, maType = "EMA")
+addBBands(n = 20, sd = 2)
+addMACD(fast = 12, slow = 26, signal = 9, type = "EMA")
+
+getSymbols("BTG", from = "2015-01-01", src = "yahoo") # 
+candleChart(BTG, subset = "2020")
+addSMA(n = 10, col = "white")
+addSMA(n = 50, col = "blue")
+addSMA(n = 200, col = "red")
+addRSI(n = 14, maType = "EMA")
+addBBands(n = 20, sd = 2)
+addMACD(fast = 12, slow = 26, signal = 9, type = "EMA")
+
+getSymbols("AGI", from = "2015-01-01", src = "yahoo") # 
+candleChart(AGI, subset = "2020")
+addSMA(n = 10, col = "white")
+addSMA(n = 50, col = "blue")
+addSMA(n = 200, col = "red")
+addRSI(n = 14, maType = "EMA")
+addBBands(n = 20, sd = 2)
+addMACD(fast = 12, slow = 26, signal = 9, type = "EMA")
+lineChart(AGI)
+# 2020-06-27 --------------------------------------------------------------
+
+getSymbols("HPQ", from = "2015-01-01", src = "yahoo") # pass
+candleChart(HPQ, subset = "2020")
+addSMA(n = 10, col = "white")
+addSMA(n = 50, col = "blue")
+addSMA(n = 200, col = "red")
+addRSI(n = 14, maType = "EMA")
+addBBands(n = 20, sd = 2)
+addMACD(fast = 12, slow = 26, signal = 9, type = "EMA")
+lineChart(HPQ)
+
+getSymbols("FSLY", from = "2015-01-01", src = "yahoo") # pass
+candleChart(FSLY, subset = "2020")
+addSMA(n = 10, col = "white")
+addSMA(n = 50, col = "blue")
+addSMA(n = 200, col = "red")
+addRSI(n = 14, maType = "EMA")
+addBBands(n = 20, sd = 2)
+addMACD(fast = 12, slow = 26, signal = 9, type = "EMA")
+lineChart(FSLY)
+
+getSymbols("PTON", from = "2015-01-01", src = "yahoo") # pass
+candleChart(PTON, subset = "2020")
+addSMA(n = 10, col = "white")
+addSMA(n = 50, col = "blue")
+addSMA(n = 200, col = "red")
+addRSI(n = 14, maType = "EMA")
+addBBands(n = 20, sd = 2)
+addMACD(fast = 12, slow = 26, signal = 9, type = "EMA")
+lineChart(PTON)
+
+getSymbols("LVGO", from = "2015-01-01", src = "yahoo") # pass
+candleChart(LVGO, subset = "2020")
+addSMA(n = 10, col = "white")
+addSMA(n = 50, col = "blue")
+addSMA(n = 200, col = "red")
+addRSI(n = 14, maType = "EMA")
+addBBands(n = 20, sd = 2)
+addMACD(fast = 12, slow = 26, signal = 9, type = "EMA")
+lineChart(LVGO)
