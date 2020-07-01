f4da3eaea03ee6506f3b6b841f8657769e8abcc8
PyRPy <44626163+PyRPy@users.noreply.github.com>
2020-07-01 09:46:52 -0500

    Create td_20200701_BA.R
    scanning stocks

---
+++ techAnalysis/td_20200701_BA.R
@@ -0,0 +1,83 @@
+# reference from youtube 
+# https://www.youtube.com/channel/UC0Amh8UkHvIVxPbxsrEBRMg
+# load packages
+library(quantmod)
+library(PerformanceAnalytics)
+# Stock watch list --------------------------------------------------------
+
+# a collection of stocks
+mystocks <- c("MSFT", "CW")
+
+# read data
+getSymbols(mystocks, from = "2015-01-01", src = "yahoo")
+# getSymbols('ZM',src='csv')
+# getSymbols('MSFT',src='csv')
+
+# Daily return and charts -------------------------------------------------
+
+# 2020-06-28 --------------------------------------------------------------
+
+candleChart(MSFT, subset = "2020")
+addSMA(n = 10, col = "white")
+addSMA(n = 50, col = "blue")
+addSMA(n = 200, col = "red")
+addRSI(n = 14, maType = "EMA")
+addBBands(n = 20, sd = 2)
+addMACD(fast = 12, slow = 26, signal = 9, type = "EMA")
+
+candleChart(CW, subset = "2020") # buy
+addSMA(n = 10, col = "white")
+addSMA(n = 50, col = "blue")
+addSMA(n = 200, col = "red")
+addRSI(n = 14, maType = "EMA")
+addBBands(n = 20, sd = 2)
+addMACD(fast = 12, slow = 26, signal = 9, type = "EMA")
+
+
+# 2020-06-29 --------------------------------------------------------------
+
+getSymbols("AMRC", from = "2015-01-01", src = "yahoo")
+candleChart(AMRC, subset = "2020") # 
+addSMA(n = 10, col = "white")
+addSMA(n = 50, col = "blue")
+addSMA(n = 200, col = "red")
+addRSI(n = 14, maType = "EMA")
+addBBands(n = 20, sd = 2)
+addMACD(fast = 12, slow = 26, signal = 9, type = "EMA")
+lineChart(AMRC)
+
+
+# 2020-06-30 --------------------------------------------------------------
+
+getSymbols("CW", from = "2015-01-01", src = "yahoo")
+candleChart(CW, subset = "2020") # 
+addSMA(n = 10, col = "white")
+addSMA(n = 50, col = "blue")
+addSMA(n = 200, col = "red")
+addRSI(n = 14, maType = "EMA")
+addBBands(n = 20, sd = 2)
+addMACD(fast = 12, slow = 26, signal = 9, type = "EMA")
+lineChart(AMRC)
+
+
+# 2020-07-01 --------------------------------------------------------------
+
+getSymbols("FMCKJ", from = "2015-01-01", src = "yahoo")
+candleChart(FMCKJ, subset = "2020") # 
+addSMA(n = 10, col = "white")
+addSMA(n = 50, col = "blue")
+addSMA(n = 200, col = "red")
+addRSI(n = 14, maType = "EMA")
+addBBands(n = 20, sd = 2)
+addMACD(fast = 12, slow = 26, signal = 9, type = "EMA")
+lineChart(AMRC)
+
+getSymbols("FDX", from = "2015-01-01", src = "yahoo")
+candleChart(FDX, subset = "2020") # 
+addSMA(n = 10, col = "white")
+addSMA(n = 50, col = "blue")
+addSMA(n = 200, col = "red")
+addRSI(n = 14, maType = "EMA")
+addBBands(n = 20, sd = 2)
+addMACD(fast = 12, slow = 26, signal = 9, type = "EMA")
+lineChart(AMRC)
