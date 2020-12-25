22b63ccba3d39413705d68492d1f3d75f954b8e0
PyRPy <44626163+PyRPy@users.noreply.github.com>
2020-12-25 13:06:54 -0500

    add cyber ETFs to search list

--- techAnalysis/template_top750_ETFs_sampler_and_screener.R
+++ techAnalysis/template_top750_ETFs_sampler_and_screener.R
@@ -5,19 +5,20 @@ library(readr)
 library(quantmod)
 library(PerformanceAnalytics)
 library(dplyr)
-companylist <- read_csv("ETFDB/etfdb_full_list.csv") # prepared in excel
+companylist <- read_csv("etfdb_full_list.csv") # prepared in excel
 colnames(companylist) <- c("Symbol", "ETF_name", "Asset_class", 
-                           "Total_assets", "YTD_Price_Change", "Daily_Volume", 
-                           "Close_price", "Pro")
+                           "Total_assets", "YTD_Price_Change", 
+                           "Daily_Volume", "Close_price", "Pro")
 head(companylist)
 # clean up
 companylist["Pro"] <- NULL
-companylist$Total_assets <- as.numeric(gsub('[$,]', '', companylist$Total_assets))
+companylist$Total_assets <- as.numeric(gsub('[$,]', '', 
+                                            companylist$Total_assets))
 companylist$Asset_class <- as.factor(companylist$Asset_class)
 
 table(companylist$Asset_class)
 companylist <- companylist %>% 
-  filter(Total_assets >= 1000 & Asset_class == "Equity")
+  filter(Total_assets >= 500 & Asset_class == "Equity")
   
 stockSymbols <- companylist["Symbol"]
 # general random sampling
@@ -25,14 +26,18 @@ numberStocks <- nrow(stockSymbols)
 idx <- sample(1:numberStocks, 10)
 
 # specific samplings on key words
-# idx_no_bond <- !grepl('Bond', companylist$Name) & !grepl('Securities', companylist$Name)
-idx_gold <- grepl('Gold', companylist$ETF_name) & !grepl('Goldman', companylist$ETF_name)
-idx_clean <- grepl('Clean', companylist$ETF_name) | grepl('Solar', companylist$ETF_name) | grepl('Battery', companylist$ETF_name)
+# idx_no_bond <- !grepl('Bond', companylist$Name) & 
+# !grepl('Securities', companylist$Name)
+idx_gold <- grepl('Gold', companylist$ETF_name) & 
+  !grepl('Goldman', companylist$ETF_name)
+idx_clean <- grepl('Clean', companylist$ETF_name) | 
+  grepl('Solar', companylist$ETF_name) | 
+  grepl('Battery', companylist$ETF_name)
 idx_ai <- grepl('Artificial', companylist$ETF_name) 
 idx_utilities <- grepl('Utilities', companylist$ETF_name) 
 idx_energy <- grepl('Energy', companylist$ETF_name) 
-
-sampledStocks <- stockSymbols[idx, "Symbol"] # add idx with sampling
+idx_cyber <- grepl('Cyber', companylist$ETF_name)
+sampledStocks <- stockSymbols[idx_cyber, "Symbol"] # add idx with sampling
 sampledStocks
 
 stocksEnv <- new.env()
@@ -46,7 +51,7 @@ for (stock in sampledStocks$Symbol) {
   candleChart(stocksEnv[[stock]], name = stock)
 }
 
-companylist_idx <- companylist[idx, ]
+companylist_idx <- companylist[idx_cyber, ]
 # Screening stocks by return and std --------------------------------------
 
 goodCandidate <- data.frame()
