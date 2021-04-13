41d4644586e143a6893e7c5f263b7c52bd25d632
PyRPy <44626163+PyRPy@users.noreply.github.com>
2021-04-13 17:39:07 -0400

    add the 'keyword' search function

--- techAnalysis/SP500_sub_industries_debug.R
+++ techAnalysis/SP500_sub_industries_debug.R
@@ -1,7 +1,24 @@
 
+companylist %>% filter(`GICS Sub-Industry` == "Movies & Entertainment") %>% 
+  select(Symbol, `GICS Sector`)
+
+movie_list <- c("FOXA", "FOX", "LYV", "NFLX", "VIAC", "DIS")
+stocksEnv <- new.env()
+getSymbols(movie_list, env = stocksEnv, 
+           from = "2021-01-01", src = "yahoo")
+
+
+for (stock in sampledStocks$Symbol) {
+  candleChart(stocksEnv[[stock]], name = stock)
+}
 
 for (sub in sort(companylist$`GICS Sub-Industry`)[1:5]) {
   cat(sub, '----- \n')
   get_data_sub(sub, '2021-01-01', 0.05, 0.01)
   
 }
+
+# find the keywords in sectors
+grep('Cable', companylist$`GICS Sub-Industry`)
+
+companylist[grep('Cable', companylist$`GICS Sub-Industry`), ]
