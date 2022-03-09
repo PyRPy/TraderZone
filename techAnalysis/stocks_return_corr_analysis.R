7cd6f8e6aa3ce8115db4c8aad9e5d6b4d23dcc53
PyRPy <44626163+PyRPy@users.noreply.github.com>
2022-03-09 10:49:56 -0500

    add portfolio analysis: monthly rebalanced

--- techAnalysis/stocks_return_corr_analysis.R
+++ techAnalysis/stocks_return_corr_analysis.R
@@ -4,27 +4,28 @@
 library(readr)
 library(quantmod)
 library(PerformanceAnalytics)
-library(fpp2)
+library(PortfolioAnalytics)
 library(corrplot)
 
 # load data from tickers --------------------------------------------------
 
-tickers = c("GNR", "HDRO", "HSY", "JETS",  "NUSI", 
-            "TCHP", "UBER", "VNM", "MO")
+tickers = c("JETS",  "NUSI", "TCHP", "UBER", "VNM", "MO")
 data_env = new.env()
 getSymbols(tickers, 
            from = "2021-01-01", 
            env = data_env, 
            auto.assign = TRUE)
-head(data_env$GNR)
+
+head(data_env$JETS)
 
 
 # Merge stock and find returns --------------------------------------------
 
 adjusted_list = lapply(data_env, Ad)
 adjusted = do.call(merge, adjusted_list)
-colnames(adjusted) = c("UBER", "JETS", "MO", "TCHP", "HSY", 
-                       "NUSI", "HDRO", "GNR", "VNM")
+adjusted= na.omit(adjusted)
+colnames(adjusted) = c("UBER", "JETS", "MO", "TCHP", 
+                       "NUSI", "VNM")
 round(cor(adjusted), 2)
 
 # HDRO is a new ETF
@@ -52,8 +53,8 @@ ret_adj = Return.calculate(adjusted)
 ret_adj = ret_adj[(-1), ] # remove the first day
 
 # boxplot for returns distributions
-boxplot(ret_adj, names = c("UBER", "JETS", "MO", "TCHP", "HSY", 
-                           "NUSI", "HDRO", "GNR", "VNM"))
+boxplot(ret_adj, names = c("UBER", "JETS", "MO", "TCHP", 
+                           "NUSI", "VNM"))
 
 
 # Return summary table ----------------------------------------------------
@@ -62,7 +63,7 @@ summary_table <- data.frame()
 
 for (stock in tickers) {
   dret <- dailyReturn(data_env[[stock]])
-  dret <- window(dret, from = "2022-02-01")
+  dret <- window(dret, from = "2022-01-01")
   pret <- Return.cumulative(dret)
   tmp <- data.frame(Symbol = stock, 
                     sd = round(sd(dret), 3), 
@@ -70,3 +71,89 @@ for (stock in tickers) {
   summary_table <- rbind(summary_table, tmp)
 }
 summary_table
+
+
+# Portfolio analysis ------------------------------------------------------
+
+# equal weight benchmark
+n <- ncol(ret_adj)
+equal_weights <- rep(1 / n, n)
+
+# start from 2021-03-11, the second day of HDRO IPO
+benchmark_returns <- Return.portfolio(R = ret_adj,
+                                      weights = equal_weights,
+                                      rebalance_on = "months")
+
+colnames(benchmark_returns) <- "benchmark"
+
+# benchmark performance
+table.AnnualizedReturns(benchmark_returns)
+
+# Base portfolio definition
+base_port_spec <- portfolio.spec(assets = colnames(ret_adj))
+base_port_spec <- add.constraint(portfolio = base_port_spec,
+                                 type = "full_investment")
+base_port_spec <- add.constraint(portfolio = base_port_spec,
+                                 type = "long_only")
+base_port_spec <- add.objective(portfolio = base_port_spec,
+                                type = "risk",
+                                name = "StdDev")
+
+# optimization backtest
+opt_base <- optimize.portfolio.rebalancing(R = ret_adj,
+                                           optimize_method = "ROI",
+                                           portfolio = base_port_spec,
+                                           rebalance_on = "months",
+                                           training_period = 12,
+                                           rolling_window = 12
+                                            )
+
+# calculate portfolio returns
+base_returns <- Return.portfolio(ret_adj,
+                                 extractWeights(opt_base))
+colnames(base_returns) <- "base"
+
+# optimization backtest
+chart.Weights(opt_base)
+
+# merge benmark and portfolio returns
+ret <- cbind(benchmark_returns, base_returns)
+
+# annualized performance
+table.AnnualizedReturns(ret)
+
+# refine constraints
+# backup of portfolio spec
+box_port_spec <- base_port_spec
+# update the constraint
+box_port_spec <- add.constraint(portfolio = box_port_spec,
+                                type = "box",
+                                min = 0.05,
+                                max = 0.4,
+                                indexnum = 2)
+
+# backtesting
+opt_box <- optimize.portfolio.rebalancing(R = ret_adj,
+                                          optimize_method = "ROI",
+                                          portfolio = box_port_spec,
+                                          rebalance_on = "months",
+                                          training_period = 12,
+                                          rolling_window = 12)
+
+# calculate portfolio returns
+box_returns <- Return.portfolio(ret_adj, extractWeights(opt_box))
+colnames(box_returns) <- "box"
+
+# chart the optimal weights
+chart.Weights(opt_box)
+
+# analysis refined constraints
+# merge box port returns
+ret <- cbind(ret, box_returns)
+# annualized performance
+table.AnnualizedReturns(ret)
+
+#                           benchmark   base    box
+# Annualized Return           -0.0490 0.0488 0.1197
+# Annualized Std Dev           0.1788 0.1159 0.1272
+# Annualized Sharpe (Rf=0%)   -0.2738 0.4205 0.9414
