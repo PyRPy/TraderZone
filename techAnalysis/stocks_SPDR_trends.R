d8bfb8347a8552eacf5a06dbf9693d87770fad67
PyRPy <44626163+PyRPy@users.noreply.github.com>
2022-03-24 20:34:30 -0500

    sectors and indicators

---
+++ techAnalysis/stocks_SPDR_trends.R
@@ -0,0 +1,27 @@
+# Sampling of stocks ------------------------------------------------------
+# https://www.cnbc.com/sector-etfs/
+library(quantmod)
+spdr_list <- c("XLE", "XLF", "XLU", "XLI", "GDX", 
+               "XLK", "XLV", "XLY", "XLP", "XLB", 
+               "XOP", "IYR", "XHB", "ITB", "VNQ",
+               "GDXJ", "IYE", "OIH", "XME", "XRT",
+               "SMH", "IBB", "KBE", "KRE", "XTL")
+
+# store data in a new environment
+stocksEnv <- new.env()
+getSymbols(spdr_list, env = stocksEnv, 
+           from = "2021-01-01", src = "yahoo")
+
+
+for (stock in spdr_list) {
+  chartSeries(stocksEnv[[stock]], 
+              theme="white", 
+              name = stock,
+              TA="addVo();
+              addBBands();
+              addRSI(14); 
+              addSMA(20, col='red'); 
+              addSMA(50, col='blue'); 
+              addSMA(200, col='black');
+              addMACD(fast=12,slow=26,signal=9,type='EMA')")
+}
