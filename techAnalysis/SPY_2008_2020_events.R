0f6353a05c475104ae6fe35652849dc44435ca06
PyRPy <44626163+PyRPy@users.noreply.github.com>
2022-07-02 13:48:34 -0500

    clean the plots, selected 2008, 2020, 2022

--- techAnalysis/SPY_2008_2020_events.R
+++ techAnalysis/SPY_2008_2020_events.R
@@ -12,17 +12,33 @@ getSymbols(sampledStocks$Symbol, env = stocksEnv,
            from = "2005-01-01", src = "yahoo")
 
 
+# Overall trend since 2005 ------------------------------------------------
+for (stock in sampledStocks$Symbol) {
+  chartSeries(window(stocksEnv[[stock]], 
+                     start = "2005-01-01", 
+                     end = "2022-07-01"),
+              theme="white", 
+              name = stock,
+              time.scale = "Monthly",
+              TA="addVo(); 
+                  addSMA(50, col='blue'); 
+                  addSMA(200, col='black')")
+}
+
 
 # 2008 crisis trend -------------------------------------------------------
 
 for (stock in sampledStocks$Symbol) {
   chartSeries(window(stocksEnv[[stock]], 
                      start = "2007-01-01", 
-                     end = "2012-01-01"),
+                     end = "2013-01-01"),
               theme="white", 
               name = stock,
-              TA="addVo();addBBands();addRSI(14); 
-            addSMA(20, col='red'); addSMA(50, col='blue'); addSMA(200, col='black')")
+              TA="addVo();
+                  addBBands();addRSI(14); 
+                  addSMA(20, col='red'); 
+                  addSMA(50, col='blue'); 
+                  addSMA(200, col='black')")
 }
 
 
@@ -35,54 +51,36 @@ for (stock in sampledStocks$Symbol) {
               theme="white", 
               name = stock,
               TA="addVo();addBBands();addRSI(14); 
-            addSMA(20, col='red'); addSMA(50, col='blue'); addSMA(200, col='black')")
-}
-
-
-# Europe crisis -----------------------------------------------------------
-for (stock in sampledStocks$Symbol) {
-  chartSeries(window(stocksEnv[[stock]], 
-                     start = "2021-01-01", 
-                     end = "2022-03-11"),
-              theme="white", 
-              name = stock,
-              TA="addVo();addBBands();addRSI(14); 
-            addSMA(20, col='red'); addSMA(50, col='blue'); addSMA(200, col='black')")
-}
-
-
-# Comparison for SMA ------------------------------------------------------
-
-for (stock in sampledStocks$Symbol) {
-  chartSeries(window(stocksEnv[[stock]], 
-                     start = "2007-01-01", 
-                     end = "2012-01-01"),
-              theme="white", 
-              name = stock,
-              TA="addBBands(); 
                   addSMA(20, col='red'); 
                   addSMA(50, col='blue'); 
                   addSMA(200, col='black')")
 }
 
-for (stock in sampledStocks$Symbol) {
-  chartSeries(window(stocksEnv[[stock]], 
-                     start = "2019-01-01", 
-                     end = "2021-01-01"),
-              theme="white", 
-              name = stock,
-              TA="addBBands(); 
-            addSMA(20, col='red'); addSMA(50, col='blue'); addSMA(200, col='black')")
-}
+
+
+# Inflation and Europe Crisis ---------------------------------------------
 
 for (stock in sampledStocks$Symbol) {
   chartSeries(window(stocksEnv[[stock]], 
                      start = "2021-01-01", 
-                     end = "2022-03-11"),
+                     end = "2022-07-01"),
               theme="white", 
               name = stock,
-              TA="addBBands();
-            addSMA(20, col='red'); addSMA(50, col='blue'); addSMA(200, col='black')")
+              TA="addVo();addBBands();addRSI(14); 
+                  addSMA(20, col='red');  
+                  addSMA(50, col='blue'); 
+                  addSMA(200, col='black')")
 }
 
-
+# Overall trend since 2005 ------------------------------------------------
+for (stock in sampledStocks$Symbol) {
+  chartSeries(window(stocksEnv[[stock]], 
+                     start = "2005-01-01", 
+                     end = "2022-07-01"),
+              theme="white", 
+              name = stock,
+              time.scale = "Monthly",
+              TA="addVo(); 
+                  addSMA(50, col='blue'); 
+                  addSMA(200, col='black')")
+}
