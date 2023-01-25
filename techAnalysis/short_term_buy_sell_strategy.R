14d6038ab1cca5f6013e854eb7841c45aa884ca5
PyRPy <44626163+PyRPy@users.noreply.github.com>
2023-01-24 22:18:32 -0600

    update S&P500 company list; add candle charts

--- techAnalysis/short_term_buy_sell_strategy.R
+++ techAnalysis/short_term_buy_sell_strategy.R
@@ -4,7 +4,7 @@ library(quantmod)
 library(PerformanceAnalytics)
 library(dplyr)
 
-companylist <- read_csv("symbols500.csv") # prepared in excel
+companylist <- read_csv("SP500_2023_01.csv") # prepared in excel
 head(companylist)
 stockSymbols <- companylist["Symbol"]
 
@@ -24,7 +24,8 @@ getSymbols(tickers,
 tickers <- filter(sampledStocks, 
                   Symbol != "INFO" & Symbol != "KSU" & Symbol != "COG" &
                     Symbol != "NLOK" & Symbol != "ANTM" & Symbol != "ABMD" &
-                    Symbol != "LB")
+                    Symbol != "LB" & Symbol != "DISCA" & Symbol != "XLNX" &
+                    Symbol != "TWTR")
 strategy_table = data.frame()
 for (stock in tickers$Symbol) {
   dret <- dailyReturn(data_env[[stock]])
@@ -42,9 +43,12 @@ for (stock in tickers$Symbol) {
       num_drop_up = num_drop_up + 1
     }
   }
+
   prob_drop_then_up = num_drop_up/num_drop
-  cat(stock, " probability drop then up ", prob_drop_then_up, " return ",pret, "\n")
+  # cat(stock, " probability drop then up ", prob_drop_then_up, " return ",pret, "\n")
   table_temp = data.frame(Symbol = stock, 
+                          num_drop = num_drop,
+                          num_drop_up = num_drop_up,
                           prob_drop_up = prob_drop_then_up,
                           cum_return = as.numeric(pret))
   strategy_table = rbind(strategy_table, table_temp)
@@ -55,5 +59,12 @@ hist(strategy_table$prob_drop_up)
 hist(strategy_table$cum_return)
 
 # select stocks with first drop and then up the next day
-stocks_potentials = subset(strategy_table, cum_return >0 & prob_drop_up > 0.70)
-write.csv(stocks_potentials, "stocks_potentials_004.csv")
+stocks_potentials = subset(strategy_table, cum_return >0 & prob_drop_up >= 0.80)
+stocks_potentials
+
+# create plots for promising stocks
+for (stock in stocks_potentials$Symbol) {
+  candleChart(data_env[[stock]], name = stock)
+}
+
+# write.csv(stocks_potentials, "stocks_potentials_004.csv")
