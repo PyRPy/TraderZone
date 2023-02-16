ceab5a77cd88e05966269fba8f710364249cf389
PyRPy <44626163+PyRPy@users.noreply.github.com>
2023-02-15 22:07:00 -0600

    add 20 day moving average

--- techAnalysis/short_term_buy_sell_strategy_v02.R
+++ techAnalysis/short_term_buy_sell_strategy_v02.R
@@ -32,6 +32,7 @@ getSymbols(tickers,
 #                     Symbol != "LB" & Symbol != "DISCA" & Symbol != "XLNX" &
 #                     Symbol != "TWTR" & Symbol != "BF.B")
 
+# remove symbols that are not downloaded
 valid_symbols = data.frame()
 for (stock in tickers) {
   if (is.null(data_env[[stock]]) == FALSE) {
@@ -39,8 +40,9 @@ for (stock in tickers) {
     valid_symbols <- rbind(valid_symbols, temp)
   }
 }
+
 strategy_table = data.frame()
-for (stock in tickers) {
+for (stock in valid_symbols$Symbol) {
   dret <- dailyReturn(data_env[[stock]])
   pret <- Return.cumulative(dret)
   dret <- coredata(dret)
@@ -58,13 +60,16 @@ for (stock in tickers) {
   }
 
   prob_drop_then_up = num_drop_up/num_drop
-  # cat(stock, " probability drop then up ", prob_drop_then_up, " return ",pret, "\n")
+  # cat(stock, " prob drop then up ", prob_drop_then_up, " return ",pret, "\n")
   table_temp = data.frame(Symbol = stock, 
-                          num_drop = num_drop,
-                          num_drop_up = num_drop_up,
-                          prob_drop_up = prob_drop_then_up,
-                          cum_return = as.numeric(pret),
-                          sd = round(sd(dret), 3))
+                    num_drop = num_drop,
+                    num_drop_up = num_drop_up,
+                    prob_drop_up = prob_drop_then_up,
+                    cum_return = as.numeric(pret),
+                    sd = round(sd(dret), 3),
+                    price_close = tail(Cl(data_env[[stock]]), 1)[[1]],
+                    SMA20 = tail(SMA(Cl(data_env[[stock]]), n = 20), 1)$SMA[[1]]
+  )
   strategy_table = rbind(strategy_table, table_temp)
   
 }
@@ -73,7 +78,10 @@ hist(strategy_table$prob_drop_up)
 hist(strategy_table$cum_return)
 
 # select stocks with first drop and then up the next day
-stocks_potentials = subset(strategy_table, cum_return >0 & prob_drop_up >= 0.80)
+# add 20-day moving average, compare with current close price
+stocks_potentials = subset(strategy_table, 
+                           cum_return >-0.1 & prob_drop_up >= 0.80 & price_close > SMA20)
+
 stocks_potentials
 
 # create plots for promising stocks
